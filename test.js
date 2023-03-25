4c5ffc7ebec4335bc87c60c01a155bc9d6c83d46
Soldy <4786022+Soldy@users.noreply.github.com>
2023-03-25 12:18:47 +0000

    future test extension
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -26,17 +26,110 @@ const output = [
     "│A2        │B2        │C2        │",
     "└──────────┴──────────┴──────────┘"
 ];
+const output_notitleseparator = [
+    "┌──────────┬──────────┬──────────┐",
+    "│testcolumA│testcolumB│testcolumC│",
+    "│A0        │B0        │C0        │",
+    "├──────────┼──────────┼──────────┤",
+    "│A1        │B1        │C1        │",
+    "├──────────┼──────────┼──────────┤",
+    "│A2        │B2        │C2        │",
+    "└──────────┴──────────┴──────────┘"
+];
+const output_notitle = [
+    "┌──────────┬──────────┬──────────┐",
+    "│A0        │B0        │C0        │",
+    "├──────────┼──────────┼──────────┤",
+    "│A1        │B1        │C1        │",
+    "├──────────┼──────────┼──────────┤",
+    "│A2        │B2        │C2        │",
+    "└──────────┴──────────┴──────────┘"
+];
+const output_noutf = [
+    "+----------+----------+----------+",
+    "|testcolumA|testcolumB|testcolumC|",
+    "+----------+----------+----------+",
+    "|A0        |B0        |C0        |",
+    "+----------+----------+----------+",
+    "|A1        |B1        |C1        |",
+    "+----------+----------+----------+",
+    "|A2        |B2        |C2        |",
+    "+----------+----------+----------+"
+];
+const output_noborder = [
+    "testcolumA│testcolumB│testcolumC",
+    "──────────┼──────────┼──────────",
+    "A0        │B0        │C0        ",
+    "──────────┼──────────┼──────────",
+    "A1        │B1        │C1        ",
+    "──────────┼──────────┼──────────",
+    "A2        │B2        │C2        "
+];
+const output_noborder_nolineseparator = [
+    "testcolumA│testcolumB│testcolumC",
+    "──────────┼──────────┼──────────",
+    "A0        │B0        │C0        ",
+    "A1        │B1        │C1        ",
+    "A2        │B2        │C2        "
+];
+
+const output_nolineseparator = [
+    "┌──────────┬──────────┬──────────┐",
+    "│testcolumA│testcolumB│testcolumC│",
+    "├──────────┼──────────┼──────────┤",
+    "│A0        │B0        │C0        │",
+    "│A1        │B1        │C1        │",
+    "│A2        │B2        │C2        │",
+    "└──────────┴──────────┴──────────┘"
+];
+const output_nocolumnseparator = [
+    "┌──────────────────────────────┐",
+    "│testcolumAtestcolumBtestcolumC│",
+    "├──────────────────────────────┤",
+    "│A0        B0        C0        │",
+    "├──────────────────────────────┤",
+    "│A1        B1        C1        │",
+    "├──────────────────────────────┤",
+    "│A2        B2        C2        │",
+    "└──────────────────────────────┘"
+];
+const output_noborder_nocolumnseparator = [
+    "testcolumAtestcolumBtestcolumC",
+    "──────────────────────────────",
+    "A0        B0        C0        ",
+    "──────────────────────────────",
+    "A1        B1        C1        ",
+    "──────────────────────────────",
+    "A2        B2        C2        "
+];
+const output_nolineseparator_nocolumnseparator = [
+    "┌──────────────────────────────┐",
+    "│testcolumAtestcolumBtestcolumC│",
+    "├──────────────────────────────┤",
+    "│A0        B0        C0        │",
+    "│A1        B1        C1        │",
+    "│A2        B2        C2        │",
+    "└──────────────────────────────┘"
+];
+
+const output_noborder_nolineseparator_nocolumnseparator = [
+    "testcolumAtestcolumBtestcolumC",
+    "──────────────────────────────",
+    "A0        B0        C0        ",
+    "A1        B1        C1        ",
+    "A2        B2        C2        "
+];
 
 
 nanoTest.add(
-    "table",
+    'default table',
     {
-        "function":table.render,
-        "options":[
+        'function':table.render,
+        'options':[
             input
         ]
     },
-    "j==",
+    'j==',
     output
 );
 nanoTest.run();
